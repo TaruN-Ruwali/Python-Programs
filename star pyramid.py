4eeccc5dcf8f81fc6e25ef95b859b2c2e596c65a
Tarun Ruwali <91328725+TaruN-Ruwali@users.noreply.github.com>
2024-11-04 20:08:21 +0530

    Delete star pyramid.py

--- star pyramid.py
+++
@@ -1,8 +0,0 @@
-n=int(input('Number of rows you want.:-'))
-for l in range(1,n+1):
-    for s in range(1,n-l+1):
-        print(' ',end='')
-    for a in range(1,2*l):
-        print('*',end='')
-    print()    
-k=input('Press to exit')
